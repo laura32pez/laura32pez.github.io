2dccfe1b96e48064b1503dc1cedf634385e7e43c
laura32pez <36621222+laura32pez@users.noreply.github.com>
2020-07-16 11:54:48 +0200

    Add files via upload

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 document.getElementById("nombreZona").style.display = 'none';
 document.getElementById("observaciones").style.display = 'none';
+document.getElementById("imagenZona").style.display = 'none';
 var canvas = document.getElementById("lienzo");
 if (canvas && canvas.getContext) {
 
@@ -17,7 +18,8 @@ if (canvas && canvas.getContext) {
 const header = document.querySelector('header');
 const section = document.querySelector('section');
 
-const requestURL = 'https://raw.githubusercontent.com/laura32pez/inPlanos/master/Vivienda1.json';
+//const requestURL = 'https://raw.githubusercontent.com/laura32pez/inPlanos/master/Vivienda1.json';
+const requestURL = 'https://raw.githubusercontent.com/laura32pez/laura32pez.github.io/gh-pages/Vivienda1IMAGEN.json';
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 
@@ -26,7 +28,6 @@ request.send();
 
 request.onload = function() {
 	const plano = request.response;
-	//populateHeader(plano);
 	
 	if (ctx) {
 		var output = document.getElementById("output");
@@ -34,25 +35,22 @@ request.onload = function() {
 		canvas.addEventListener("mousemove", function (evt) {
 			var mousePos = oMousePos(canvas, evt);
 			showZonas(plano,mousePos.x, mousePos.y);
+			//marcarCoords(output, mousePos.x, mousePos.y);
 		}, false);
 
+		canvas.addEventListener("mouseout", function(evt) {
+			limpiarCoords(output);
+		  }, false);
 
 	}
   }
 
  
-/*
-  function populateHeader(jsonObj) {
-	const myH1 = document.createElement('h1');
-	myH1.textContent = jsonObj['NombrePlano'];
-	header.appendChild(myH1).style.display = 'block';
-
-  }
-  */
 
   function showZonas(jsonObj,x,y) {
 	document.getElementById("nombreZona").style.display = 'none';
 	document.getElementById("observaciones").style.display = 'none';
+	document.getElementById("imagenZona").style.display = 'none';
     esZona = false;
 	const zonas = jsonObj['Zonas'];
 
@@ -65,15 +63,20 @@ request.onload = function() {
 	  if(x1<=x && x<=x2 && y1<=y && y<=y2){
 		var nombre = document.getElementById("nombreZona");
 		var observaciones = document.getElementById("observaciones");
+		var imagen = document.getElementById("imagenZona");
 		nombre.textContent = zonas[i].nombre;
 		observaciones.textContent = zonas[i].observaciones;
+		imagen.src = zonas[i].imagen;
 		document.getElementById("nombreZona").style.display = 'block';
 		document.getElementById("observaciones").style.display = 'block';
-		ctx.beginPath();
-		ctx.lineWidth = "5";
-		ctx.strokeStyle = "blue";
-		ctx.rect(zonas[i].x3, zonas[i].y3, zonas[i].wi, zonas[i].he);
-		ctx.stroke();
+		document.getElementById("imagenZona").style.display = 'block';
+		ctx.globalAlpha = 0.2;
+		//ctx.beginPath();
+		//ctx.lineWidth = "5";
+		ctx.fillStyle =  zonas[i].color;
+		//ctx.strokeStyle = "red" ;
+		ctx.fillRect(zonas[i].x3, zonas[i].y3, zonas[i].wi, zonas[i].he);
+		//ctx.stroke();
 		esZona = true;
 	  }
 	  
@@ -93,4 +96,22 @@ function oMousePos(canvas, evt) {
 }
 
 
+
+function marcarCoords(output, x, y) {
+	output.innerHTML = ("x: " + x + ", y: " + y);
+	output.style.top = (y + 10) + "px";
+	output.style.left = (x + 10) + "px";
+	output.style.backgroundColor = "#FFF";
+	output.style.border = "1px solid #d9d9d9"
+	canvas.style.cursor = "pointer";
+  }
+  
+  function limpiarCoords(output) {
+	output.innerHTML = "";
+	output.style.top = 0 + "px";
+	output.style.left = 0 + "px";
+	output.style.backgroundColor = "transparent"
+	output.style.border = "none";
+	canvas.style.cursor = "default";
+  }
 
